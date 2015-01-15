3f87f077b1bcaa68092782e34624b31ec0257a6f
Minas Anastasi <minasanastasi@Minass-MacBook-Pro.local>
2015-01-15 10:44:56 +0000

    added two_cities.txt

--- add.c
+++ add.c
@@ -3,12 +3,26 @@
 
 int main()
 {
-	int a,b,total;
-	printf("Enter two numbers:");
-	scanf("%i" "%i",&a,&b);
+	int a,b, average;
+	int max;
+	int min = 1;
+	int total = 0;
 	
-	total = a+b;
+	printf("Enter number of entries\n");
+	scanf("%i",&max);
+	
+	if (max<=5 && min>=1){
+		for(int i=0; i<max; i++){
+		printf("Enter two numbers:");
+		scanf("%i" "%i",&a,&b);
+		total = total+(a+b);
+		}
+	}else printf("Not in range\n");
+	
+	average = total/max;
+	
 	printf("Add = %i\n",total);
+	printf("Average = %i\n",average);
 	
 
 	return 0;
